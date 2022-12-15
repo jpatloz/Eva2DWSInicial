4e8d6dcf025545235fe486341d4fca29797c2940
DESKTOP-3I3N22C\JesusPatricio <csi22-jpatano@colegioaltair.net>
2022-12-15 12:54:14 +0100

    Cambios en los controladores

--- Eva2DWSInicial/Controllers/EvaCatEvaluacionsController.cs
+++ Eva2DWSInicial/Controllers/EvaCatEvaluacionsController.cs
@@ -12,6 +12,9 @@ namespace Eva2DWSInicial.Controllers
 {
     public class EvaCatEvaluacionsController : Controller
     {
+
+        //En este controlador hacemos el create necesario para la vista
+
         private readonly BdEvaluacionContext _context;
 
         public EvaCatEvaluacionsController(BdEvaluacionContext context)
@@ -22,7 +25,7 @@ namespace Eva2DWSInicial.Controllers
         // GET: EvaCatEvaluacions
         public async Task<IActionResult> Index()
         {
-              return View(await _context.EvaCatEvaluacions.ToListAsync());
+            return View(await _context.EvaCatEvaluacions.ToListAsync());
         }
 
         // GET: EvaCatEvaluacions/Details/5
@@ -41,121 +44,6 @@ namespace Eva2DWSInicial.Controllers
             }
 
             return View(evaCatEvaluacion);
-        }
-
-        // GET: EvaCatEvaluacions/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: EvaCatEvaluacions/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CodEvaluacion,DescEvaluacion")] EvaCatEvaluacion evaCatEvaluacion)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(evaCatEvaluacion);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(evaCatEvaluacion);
-        }
-
-        // GET: EvaCatEvaluacions/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null || _context.EvaCatEvaluacions == null)
-            {
-                return NotFound();
-            }
-
-            var evaCatEvaluacion = await _context.EvaCatEvaluacions.FindAsync(id);
-            if (evaCatEvaluacion == null)
-            {
-                return NotFound();
-            }
-            return View(evaCatEvaluacion);
-        }
-
-        // POST: EvaCatEvaluacions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("CodEvaluacion,DescEvaluacion")] EvaCatEvaluacion evaCatEvaluacion)
-        {
-            if (id != evaCatEvaluacion.CodEvaluacion)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(evaCatEvaluacion);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EvaCatEvaluacionExists(evaCatEvaluacion.CodEvaluacion))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(evaCatEvaluacion);
-        }
-
-        // GET: EvaCatEvaluacions/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null || _context.EvaCatEvaluacions == null)
-            {
-                return NotFound();
-            }
-
-            var evaCatEvaluacion = await _context.EvaCatEvaluacions
-                .FirstOrDefaultAsync(m => m.CodEvaluacion == id);
-            if (evaCatEvaluacion == null)
-            {
-                return NotFound();
-            }
-
-            return View(evaCatEvaluacion);
-        }
-
-        // POST: EvaCatEvaluacions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            if (_context.EvaCatEvaluacions == null)
-            {
-                return Problem("Entity set 'BdEvaluacionContext.EvaCatEvaluacions'  is null.");
-            }
-            var evaCatEvaluacion = await _context.EvaCatEvaluacions.FindAsync(id);
-            if (evaCatEvaluacion != null)
-            {
-                _context.EvaCatEvaluacions.Remove(evaCatEvaluacion);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool EvaCatEvaluacionExists(string id)
-        {
-          return _context.EvaCatEvaluacions.Any(e => e.CodEvaluacion == id);
         }
     }
 }
--- Eva2DWSInicial/Controllers/EvaTchNotasEvaluacionsController.cs
+++ Eva2DWSInicial/Controllers/EvaTchNotasEvaluacionsController.cs
@@ -12,6 +12,8 @@ namespace Eva2DWSInicial.Controllers
 {
     public class EvaTchNotasEvaluacionsController : Controller
     {
+        //En este controlador hacemos el create necesario para la vista
+
         private readonly BdEvaluacionContext _context;
 
         public EvaTchNotasEvaluacionsController(BdEvaluacionContext context)
@@ -24,25 +26,6 @@ namespace Eva2DWSInicial.Controllers
         {
             var bdEvaluacionContext = _context.EvaTchNotasEvaluacions.Include(e => e.CodEvaluacionNavigation);
             return View(await bdEvaluacionContext.ToListAsync());
-        }
-
-        // GET: EvaTchNotasEvaluacions/Details/5
-        public async Task<IActionResult> Details(long? id)
-        {
-            if (id == null || _context.EvaTchNotasEvaluacions == null)
-            {
-                return NotFound();
-            }
-
-            var evaTchNotasEvaluacion = await _context.EvaTchNotasEvaluacions
-                .Include(e => e.CodEvaluacionNavigation)
-                .FirstOrDefaultAsync(m => m.IdNotaEvaluacion == id);
-            if (evaTchNotasEvaluacion == null)
-            {
-                return NotFound();
-            }
-
-            return View(evaTchNotasEvaluacion);
         }
 
         // GET: EvaTchNotasEvaluacions/Create
@@ -67,102 +50,6 @@ namespace Eva2DWSInicial.Controllers
             }
             ViewData["CodEvaluacion"] = new SelectList(_context.EvaCatEvaluacions, "CodEvaluacion", "CodEvaluacion", evaTchNotasEvaluacion.CodEvaluacion);
             return View(evaTchNotasEvaluacion);
-        }
-
-        // GET: EvaTchNotasEvaluacions/Edit/5
-        public async Task<IActionResult> Edit(long? id)
-        {
-            if (id == null || _context.EvaTchNotasEvaluacions == null)
-            {
-                return NotFound();
-            }
-
-            var evaTchNotasEvaluacion = await _context.EvaTchNotasEvaluacions.FindAsync(id);
-            if (evaTchNotasEvaluacion == null)
-            {
-                return NotFound();
-            }
-            ViewData["CodEvaluacion"] = new SelectList(_context.EvaCatEvaluacions, "CodEvaluacion", "CodEvaluacion", evaTchNotasEvaluacion.CodEvaluacion);
-            return View(evaTchNotasEvaluacion);
-        }
-
-        // POST: EvaTchNotasEvaluacions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("MdUuid,MdFch,IdNotaEvaluacion,CodAlumno,NotaEvaluacion,CodEvaluacion")] EvaTchNotasEvaluacion evaTchNotasEvaluacion)
-        {
-            if (id != evaTchNotasEvaluacion.IdNotaEvaluacion)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(evaTchNotasEvaluacion);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EvaTchNotasEvaluacionExists(evaTchNotasEvaluacion.IdNotaEvaluacion))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CodEvaluacion"] = new SelectList(_context.EvaCatEvaluacions, "CodEvaluacion", "CodEvaluacion", evaTchNotasEvaluacion.CodEvaluacion);
-            return View(evaTchNotasEvaluacion);
-        }
-
-        // GET: EvaTchNotasEvaluacions/Delete/5
-        public async Task<IActionResult> Delete(long? id)
-        {
-            if (id == null || _context.EvaTchNotasEvaluacions == null)
-            {
-                return NotFound();
-            }
-
-            var evaTchNotasEvaluacion = await _context.EvaTchNotasEvaluacions
-                .Include(e => e.CodEvaluacionNavigation)
-                .FirstOrDefaultAsync(m => m.IdNotaEvaluacion == id);
-            if (evaTchNotasEvaluacion == null)
-            {
-                return NotFound();
-            }
-
-            return View(evaTchNotasEvaluacion);
-        }
-
-        // POST: EvaTchNotasEvaluacions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(long id)
-        {
-            if (_context.EvaTchNotasEvaluacions == null)
-            {
-                return Problem("Entity set 'BdEvaluacionContext.EvaTchNotasEvaluacions'  is null.");
-            }
-            var evaTchNotasEvaluacion = await _context.EvaTchNotasEvaluacions.FindAsync(id);
-            if (evaTchNotasEvaluacion != null)
-            {
-                _context.EvaTchNotasEvaluacions.Remove(evaTchNotasEvaluacion);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool EvaTchNotasEvaluacionExists(long id)
-        {
-          return _context.EvaTchNotasEvaluacions.Any(e => e.IdNotaEvaluacion == id);
         }
     }
 }
